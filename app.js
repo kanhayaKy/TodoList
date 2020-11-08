5f045d37ea64be59f505f09d1fbe619f0847856b
kanhayaKy <kaku18is@cmrit.ac.in>
2020-11-08 23:58:08 +0530

    Added deployment files

--- app.js
+++ app.js
@@ -110,6 +110,12 @@ app.get('/:listName', function(req, res){
 
 
 
-app.listen(3000, function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function(){
     console.log("Server has started at local host 3000");
 })
